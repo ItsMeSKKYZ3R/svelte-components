7adb612923f4b665e828a5ec851cdd568f3c620e
Coyotte93 <71552277+ItsMeSKKYZ3R@users.noreply.github.com>
2023-06-28 14:08:59 +0200

    First component without styles
    Components in dev. Do not use in dev.

---
+++ src/components/Button.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    export let text: string | number | boolean;
+    export let type: "button" | "submit" | "reset" | null | undefined = undefined;
+    // export let onclick: any = undefined;
+    export let style: "modern" | "round";
+</script>
+
+<button {type} class={style} on:click on:dblclick>{text}</button>
+
+<style>
+    button {
+        border: none;
+    }
+    .modern {
+        color: red;
+    }
+</style>
